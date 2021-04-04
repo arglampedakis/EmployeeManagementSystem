1d58cae75f9b67182f5dcd4a948fa44f93292464
a_glampedakis <61798869+arglampedakis@users.noreply.github.com>
2021-04-04 17:53:18 +0300

    added getAttributeListByEmpId in employeee controller

--- src/main/java/com/cite/employee_managment/controller/EmployeeController.java
+++ src/main/java/com/cite/employee_managment/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.cite.employee_managment.controller;
 
+import com.cite.employee_managment.dto.AttributeDto;
 import com.cite.employee_managment.dto.EmployeeDto;
+import com.cite.employee_managment.mapper.AttributeMapper;
+import com.cite.employee_managment.model.Attribute;
 import com.cite.employee_managment.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,17 +16,26 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/employee")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST})
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+    private final AttributeMapper attributeMapper;
 
-    @GetMapping(path = "/{empId}", produces = {"application/json"})
+    @GetMapping(path = "/{empId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EmployeeDto> getById(@PathVariable("empId") Integer empId) {
         return new ResponseEntity<>(employeeService.findById(empId), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/", produces = {"application/json"})
+    @GetMapping(path = "/attributes/{empId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<AttributeDto>> getEmployeeAttributesByEmpId(@PathVariable("empId") Integer empId) {
+        return new ResponseEntity<>(
+                attributeMapper.attributesToAttributeDtos(
+                        (List<Attribute>) employeeService.findEmployeeById(empId).getAttributesCollection())
+                , HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<EmployeeDto>> getAll() {
         return new ResponseEntity<>(employeeService.findAll(), HttpStatus.OK);
     }
